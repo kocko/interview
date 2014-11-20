fad347508cdb487e31c28f58faace0c56460bb1d
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2014-11-20 15:08:55 +0200

    Slight modifications

--- src/main/java/interview/impl/InvoiceHandlerImpl.java
+++ src/main/java/interview/impl/InvoiceHandlerImpl.java
@@ -1,6 +1,7 @@
 package interview.impl;
 
 import interview.InvoiceHandler;
+import interview.Writer;
 import interview.bean.Invoice;
 import interview.csv.CsvInvoicesParser;
 import interview.csv.CsvInvoicesWriter;
@@ -12,13 +13,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class InvoiceHandlerImpl implements InvoiceHandler {
+	
+	private CsvInvoicesWriter csvWriter;
+	private XmlInvoicesWriter xmlWriter;
+	
+	public InvoiceHandlerImpl() {
+		csvWriter = new CsvInvoicesWriter();
+		xmlWriter = new XmlInvoicesWriter();
+	}
 
 	public boolean ingestInvoiceData(File file, Set<OutputFormat> outputFormats) throws IllegalArgumentException {
-		if (outputFormats == null || outputFormats.size() == 0) {
-			throw new IllegalArgumentException("You have to provide non-null and non-empty list of output formats!");
+		Objects.requireNonNull(file, "You have to provide a non-null reference to a file");
+		Objects.requireNonNull(outputFormats, "You have to provice a non-null list of output formats");
+		if (outputFormats.size() == 0) {
+			throw new IllegalArgumentException("You have to provide a non-empty list of output formats!");
 		}
 		
 		Map<String, List<Invoice>> parsedData = null;
@@ -29,19 +42,14 @@ public class InvoiceHandlerImpl implements InvoiceHandler {
 			e.printStackTrace();
 		}
 		
-		CsvInvoicesWriter csvWriter = new CsvInvoicesWriter();
-		XmlInvoicesWriter xmlWriter = new XmlInvoicesWriter();
-		
 		Boolean result = null;
 		
 		if (parsedData != null && FileUtils.makeOutputDir()) {
 			for (OutputFormat format: outputFormats) {
-				if (format.equals(OutputFormat.CSV)) {
-					csvWriter.process(parsedData);
-				}
-				
-				if (format.equals(OutputFormat.XML)) {
-					xmlWriter.process(parsedData);
+				Optional<Writer<?>> writerExistence = getWriterForOutputFormat(format);
+				if (writerExistence.isPresent()) {
+					Writer<?> writer = writerExistence.get();
+					writer.process(parsedData);
 				}
 			}
 			result = Boolean.TRUE;
@@ -49,6 +57,19 @@ public class InvoiceHandlerImpl implements InvoiceHandler {
 			result = Boolean.FALSE;
 		}
 		
+		return result;
+	}
+	
+	private Optional<Writer<?>> getWriterForOutputFormat(OutputFormat outputFormat) {
+		Optional<Writer<?>> result = Optional.empty();
+		switch (outputFormat){
+			case CSV: {
+				result = Optional.of(csvWriter);
+			}
+			case XML: {
+				result = Optional.of(xmlWriter); 
+			}
+		}
 		return result;
 	}
 }
